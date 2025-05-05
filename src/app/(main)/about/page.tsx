7c897be044d9d1f8f7d46a6bdc5a230a585164e0
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2025-05-05 21:11:04 +0545

    routing metadata

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+    title: 'About',
+    description: 'Created',
+};
+
 export default function About() {
-    return <h1>About</h1>
+    return <h1>About</h1>;
 }
--- src/app/(main)/products/[productId]/page.tsx
+++ src/app/(main)/products/[productId]/page.tsx
@@ -1,9 +1,30 @@
-export default async function ProductDetails ({
+import { Metadata } from 'next';
+
+type Props = {
+    params: Promise<{ productId: string }>;
+};
+
+export const generateMetadata = async ({
+    params,
+}: Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iPhone ${id}`);
+        }, 100);
+    });
+
+    return {
+        title: `Product ${title}`,
+    };
+};
+
+export default async function ProductDetails({
     params,
 }: {
     params: Promise<{ productId: string }>;
 }) {
     const productId = (await params).productId;
-    console.log("productId", productId)
-    return <h1>Product Details { productId }</h1>;
+    console.log('productId', productId);
+    return <h1>Product Details {productId}</h1>;
 }
