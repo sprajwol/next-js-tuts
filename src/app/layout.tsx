ea35ce00d67ad219cfcaec21e8ea29f040588b50
Prajwol Man Shakya <shakyaprajwol96@gmail.com>
2025-05-05 15:57:21 +0545

    layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,32 @@
+export const metadata = {
+    title: 'Next.js',
+    description: 'Created',
+};
+
 export default function RootLayout({
-    children
+    children,
 }: {
-    children: React.ReactNode
+    children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-        <body>
-            <main>{children}</main>
-        </body>
+        <html lang='en'>
+            <body>
+                <header
+                    style={{
+                        backgroundColor: 'lightblue',
+                        padding: '1rem',
+                    }}>
+                    Header
+                </header>
+                <main>{children}</main>
+                <footer
+                    style={{
+                        backgroundColor: 'ghostwhite',
+                        padding: '1rem',
+                    }}>
+                    Footer
+                </footer>
+            </body>
         </html>
-    )
+    );
 }
